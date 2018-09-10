3b62134bd5be1243050bbae2056468328073fc83
Paul Ashour <paul.ashour@gmail.com>
2018-09-10 02:31:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  
+  temp = { object };
+  delete temp[key];
+  return temp;
 }
